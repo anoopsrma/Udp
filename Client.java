65f931e1e1ebaf3fe9dc256c7fb8af125f471922
anup-seismo <navyanup1@gmail.com>
2016-04-23 22:58:04 +0545

    I don't know what i am doing :)

--- Client.java
+++ Client.java
@@ -1,37 +1,73 @@
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
-public class Client
+class Client
 {
-static InetAddress dest;
-public static void main(String [] args) throws Exception
-{
+static int serverPort;
+static String filename;
+    public static void main(String args[]) throws SocketException, IOException
+    {
+        int count=0;
+        int MAX_SIZE = 1048;
 
-    DatagramSocket clskt = new DatagramSocket();
-    Scanner input = new Scanner (System.in);
-    int port =input.nextInt();
-    System.out.println("Enter Destination Host name");
-    String hostname=input.next();
-    dest.getByName(hostname);
-    int packetcount=0;
-    System.out.println("Enter The path of the file you want to send");
-    String path = input.next(); 
-    File initialFile = new File(path);
-            FileInputStream targetStream = new FileInputStream(initialFile);
-    int filesize=targetStream.available();
-    //int neededpackets =(int)Math.ceil((double)(size/1024));
-     byte [] data= new byte[1024];
-     // counting bytes
-     for (int i=0;i<1024;i++)
-     {
-         data[i]=(byte)targetStream.read();
-     }
-     //create a packet
-    DatagramPacket clpkt=new DatagramPacket(data,data.length,dest,port);
-    packetcount++;
-    clskt.send(clpkt);
-    if(packetcount >neededpackets)
-        clskt.close();
-   }
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IpAddress = InetAddress.getByName("localhost");
 
- }
+        byte[] sendData = new byte[MAX_SIZE];
+
+        String filePath = "/home/test.txt";
+        File file = new File(filePath);
+        FileInputStream fis = new FileInputStream(file);
+
+
+        int totLength = 0; 
+
+        while((count = fis.read(sendData)) != -1)    //calculate total length of file
+        {
+            totLength += count;
+        }
+
+        System.out.println("Total Length :" + totLength);
+
+        int noOfPackets = totLength/MAX_SIZE;
+        System.out.println("No of packets : " + noOfPackets);
+
+        int off = noOfPackets * MAX_SIZE;  //calculate offset. it total length of file is 1048 and array size is 1000 den starting position of last packet is 1001. this value is stored in off.
+
+        int lastPackLen = totLength - off;
+        System.out.println("\nLast packet Length : " + lastPackLen);
+
+        byte[] lastPack = new byte[lastPackLen-1];  //create new array without redundant information
+
+
+        fis.close();
+
+        FileInputStream fis1 = new FileInputStream(file);
+        while((count = fis1.read(sendData)) != -1 )
+        { 
+            if(noOfPackets<=0)
+            break;
+            System.out.println(new String(sendData));
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IpAddress, 9876);
+            clientSocket.send(sendPacket);
+            System.out.println("========");
+            System.out.println("last pack sent" + sendPacket);
+            noOfPackets--;
+        }
+
+        //check
+        System.out.println("\nlast packet\n");
+        System.out.println(new String(sendData));
+
+        lastPack = Arrays.copyOf(sendData, lastPackLen);
+
+        System.out.println("\nActual last packet\n");
+        System.out.println(new String(lastPack));
+                //send the correct packet now. but this packet is not being send. 
+        DatagramPacket sendPacket1 = new DatagramPacket(lastPack, lastPack.length, IpAddress,     9876);
+        clientSocket.send(sendPacket1);
+        System.out.println("last pack sent" + sendPacket1);
+
+    }
+}
--- Server.java
+++ Server.java
@@ -1,40 +1,26 @@
 import java.io.*;
- import java.net.*;
- import java.util.*;
-
- class Server
-   {
-public static void main(String args[])throws Exception
+import java.net.*;
+class Server
 {
-    System.out.println("Enter Port number !!!");
-    Scanner input = new Scanner(System.in);
-    int SPort = input.nextInt();
-    DatagramSocket srvskt = new DatagramSocket(SPort);
-    byte[] data =new byte[1024];
-    System.out.println("Enter a full file name to save data to it ?");
-    String path = input.next();
-    System.out.println("file : "+path+" will be created.");
-    FileOutputStream  FOS = new FileOutputStream(path);
-    DatagramPacket srvpkt = new DatagramPacket(data,1024);
-    System.out.println("listening to Port: "+SPort);
-    int Packetcounter=0;//packet counter
-    while(true)
-       {
-           srvskt.receive(srvpkt);
-           Packetcounter++;
-           String words = new String(srvpkt.getData());
-           InetAddress ip= srvpkt.getAddress();
-           int port = srvpkt.getPort();
-           System.out.println("Packet # :"+Packetcounter+"
-            Received from Host / Port: "+ip+" / "+port);
-           FOS.write(data);
-           //out16.flush();
-           if (Packetcounter >=100)
-                 break;
+    public static void main(String args[]) throws IOException
+    {
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        byte[] recData = new byte[1024];
+        int i =0;
 
-      }
-    FOS.close();//releasing file.
-    System.out.println("Data has been written to the file !");
-  }
+        FileWriter file = new FileWriter("/home/out.txt");
+        PrintWriter out = new PrintWriter(file);
+
+        while(true)
+        {
+            DatagramPacket recPacket = new DatagramPacket(recData, recData.length);
+            serverSocket.receive(recPacket);
+            System.out.println("\n Packet length: " + recPacket.getLength());
+            String line = new String(recPacket.getData(), 0, recPacket.getLength());
+            System.out.println("\n Data: " + line);
+            out.println(line);
+            System.out.println("\nPacket" + ++i + " written to file\n");
+            out.flush();
+        }
+    }
 }
-
